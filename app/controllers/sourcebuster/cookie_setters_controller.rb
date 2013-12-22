99bc6c6666ee5c89852c64adedc93e1cc70197fc
Alex Fedoseev <alex.fedoseev@gmail.com>
2013-12-22 09:40:24 +0000

    Remove test layouts from engine

--- app/controllers/sourcebuster/cookie_setters_controller.rb
+++ app/controllers/sourcebuster/cookie_setters_controller.rb
@@ -2,9 +2,5 @@ require_dependency "sourcebuster/application_controller"
 
 module Sourcebuster
   class CookieSettersController < ApplicationController
-
-		def showoff
-		end
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Sourcebuster::Engine.routes.draw do
 	get '/', to: redirect { |params, request| "/sourcebuster/showoff#{'?' unless request.params.to_query.blank?}#{request.params.to_query}" }
-	get '/showoff', to: 'cookie_setters#showoff', as: 'showoff'
 end
